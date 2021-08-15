e657243ed0149488e2a69651f6560f2e05011483
dangeon master 300$ <vip.danko1521@mail.ru>
2021-08-15 20:35:52 +0300

    table views +

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,103 @@
+import csv
 from django.shortcuts import render
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
+    rows = []
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
+    with open('inflation_russia.csv', encoding='utf-8') as isfile:
+        file = csv.reader(isfile, delimiter=';')
+        linecount = 0
+        for row in file:
+            if linecount == 0:
+                header = row
+                linecount += 1
+            else:
+                converted_data = [row[0]]
+                for i in row[1:]:
+                    try:
+                        converted_data.append(float(i))
+                    except:
+                        converted_data.append('-')
+                rows.append(converted_data)
+
+
+    context = {'data': rows, 'header':header}
+
+
+
 
     return render(request, template_name,
-                  context)
+                  context=context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import csv
+# import io
+#
+# from django.shortcuts import render
+#
+#
+# def inflation_view(request):
+#     template_name = 'inflation.html'
+#     rows = []
+#
+#     with io.open('inflation_russia.csv', encoding='utf-8') as isfile:
+#         file = csv.reader(isfile, delimiter=';')
+#         linecount = 0
+#         for row in file:
+#             if linecount == 0:
+#                 header = row
+#                 linecount += 1
+#             else:
+#                 converted_data = [row[0]]
+#                 for i in row[1:]:
+#                     try:
+#                         converted_data.append(float(i))
+#                     except:
+#                         converted_data.append('-')
+#                 rows.append(converted_data)
+#
+#     # чтение csv-файла и заполнение контекста
+#     context = {'data': rows, 'header': header}
+#
+#     return render(request, template_name,
+#                   context=context)
